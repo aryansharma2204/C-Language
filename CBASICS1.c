6958d6b019ba98fa1279c1cef09983e9e685f5ba
aryansharma2204 <aryansde2204@gmail.com>
2022-11-13 02:08:04 +0530

    Create CBASICS1.c

---
+++ CBASICS1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("Hello World\n");
+    /* code */
+    return 0;
+}
+
+/*
+#include<stdio.h> is a pre processor command, that means at the time of pre processing our compiler will deal with it.
+This command simply tells the compiler that we need a stdio.h file in this program. After we apply this file in the program then things will happen in this program.
+That means we need stdio.h please include this in program
+
+int main() This is a function. Roughly- To break a program in parts we use function. int main() is a special type of function because execution of program starts from here that means our program start from there.
+our program will start it will get inside int main() then whatever calls will come to function it will go there.
+main is the name of the function, int is that integer which will return the function. Every function has a return value that means when you call that function it gives something to you in return.
+The function main() calls different functions and those functions gives main() something in return and that value is used by it. ex: a + b = c now it will give this c in return to main()
+Why return value of main() is int because it also returns a number to whom, to whom he called 
+main() function will return int and that int will go to operating system
+
+printf(); it is a different function which we are using from stdio.h header file, we included stdio.h to use this printf() and by using this we print in console th desired output like here its hello world as a string
+
+return 0; this will give a integer in return to the operating system and 0 means, If everything is fine then this will give 0 in return to the Operating System
+*/
